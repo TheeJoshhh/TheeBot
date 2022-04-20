545aab3eda598df91889223df834dfe094b03a6a
Joshua Billing <joshua.billing9@gmail.com>
2022-04-20 17:05:12 +1200

    Tidied up the code.

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,4 @@
-const { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require('discord.js');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { color_theme, website, discord_server } = require('../config.json');
 const { version, homepage } = require('../package.json');
@@ -14,41 +14,45 @@ module.exports = {
     run: function (client, interaction) {
         // HANDLE COMMANDS.
         if (interaction.isCommand()) {
-            const aboutString = 
-            `
-            ${client.user.username} is a bot made by TheeJoshhh#9320.
-            This bot doesn't have any one purpose besides being a fun project to work on.
-            `
+            // Create the embed that'll be sent to the user.
             const embed = new MessageEmbed()
                 .setTitle(`Information About ${client.user.username}`)
                 .setColor(color_theme)
                 .setURL(website)
-                .addField('Description', aboutString)
+                .addField(
+                    'Description', 
+                    `${client.user.username} is a bot made by TheeJoshhh#9320.
+                    This bot doesn't have any one purpose besides being a fun project to work on.`,
+                    false
+                )
                 .addField('Servers', `${client.guilds.cache.size}`, true)
                 .addField('Uptime', `${ms(client.uptime, { long: true })}`, true)
                 .setFooter({ text: `${client.user.username} V${version}`, iconURL: client.user.avatarURL() })
 
-                const row = new MessageActionRow()
-                    .addComponents(
-                        new MessageButton()
-                            .setLabel('GitHub')
-                            .setStyle('LINK')
-                            .setURL(homepage)
-                            .setEmoji('<:GitHub:966170492547530752>')
-                    )
-                    .addComponents(
-                        new MessageButton()
-                            .setLabel('Discord Server')
-                            .setStyle('LINK')
-                            .setURL(discord_server)
-                            .setEmoji('<:Discord:966180598219698196>')
-                    )
-                    .addComponents(
-                        new MessageButton()
-                            .setLabel('Website')
-                            .setStyle('LINK')
-                            .setURL(website)
-                    )
+            // Create the buttons that will show underneath the embed.
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('Website')
+                        .setStyle('LINK')
+                        .setURL(website)
+                )
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('GitHub')
+                        .setStyle('LINK')
+                        .setURL(homepage)
+                        .setEmoji('<:GitHub:966170492547530752>')
+                )
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('Discord Server')
+                        .setStyle('LINK')
+                        .setURL(discord_server)
+                        .setEmoji('<:Discord:966180598219698196>')
+                )
+
+            // Send the embed and buttons.
             interaction.reply({
                 embeds: [embed],
                 components: [row]
