0022e56817b30551ccb287a1957cfc02fde445b1
Joshua Billing <joshua.billing9@gmail.com>
2022-04-20 22:04:57 +1200

    Added CheckMod Function

--- utils.js
+++ utils.js
@@ -1,8 +1,10 @@
 // Import required modules.
 const fs = require('fs');
+const Moderators = require('./schemas/moderator');
 
 // Export utils.
 module.exports.getFiles = getFiles;
+module.exports.checkMod = checkMod;
 
 // Returns all files from a given directory and all sub-directories.
 function getFiles(dir) {
@@ -22,4 +24,16 @@ function getFiles(dir) {
         }
     }
     return commandFiles;
+}
+
+function checkMod(member) {
+    if (member.permissions.has('MODERATE_MEMBERS')) return true;
+    const mod_data = Moderators.findOne({ _id: member.guild.id });
+    if (mod_data) {
+        if (mod_data.users.includes(member.user.id)) return true;
+        for (x = 0; x < mod_data.roles.length; x++) {
+            if (member.roles.cache.includes(mod_data.roles[x])) return true;
+        }
+    }
+    return false;
 }
