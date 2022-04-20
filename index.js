03d3c25961adf0296d03f309f51bd6292a862903
Joshua Billing <joshua.billing9@gmail.com>
2022-04-20 18:36:53 +1200

    Added database connection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // Import required modules.
 const Discord = require('discord.js');
+const mongoose = require('mongoose');
 const { Intents } = Discord;
-const { production_mode, production_token, development_token } = require('./config.json');
+const { 
+    production_mode, 
+    production_token, 
+    development_token, 
+    production_mongo_uri, 
+    development_mongo_uri 
+} = require('./config.json');
 const command_handler = require('./command-handler');
+
+
 
 // Create the discord.js client.
 const client = new Discord.Client({
@@ -13,7 +22,13 @@ const client = new Discord.Client({
 });
 
 // Ready Event (Fired when the bot is online and ready).
-client.on('ready', () => {
+client.on('ready', async () => {
+    // Connect the database.
+    console.log('Connecting to the Database...');
+    await mongoose.connect(production_mode ? production_mongo_uri : development_mongo_uri);
+    console.log('Database Connected!');
+    
+    // Load the command handler.
     command_handler(client);
     console.log(`${client.user.username} is now online and ready to receive commands!`);
 });
