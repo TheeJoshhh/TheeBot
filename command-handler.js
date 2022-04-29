cd151e87b5107e7b6e7d0e2315ee7015712c8d1e
Joshua Billing <joshua.billing9@gmail.com>
2022-04-30 01:43:06 +1200

    Changed the way slash commands are registered

--- command-handler.js
+++ command-handler.js
@@ -1,10 +1,14 @@
 // Import required modules.
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { production_mode, development_guilds } = require('./config.json');
 const { getFiles } = require('./utils.js');
 
 module.exports = (client) => {
     // An object that'll hold all of the commands.
     const commands = {};
+    // An array of data to be sent to the discord API.
+    const commandAPIData = [];
 
     // Get a list of all of the files in the commands folder.
     const commandFiles = getFiles('./commands');
@@ -18,25 +22,32 @@ module.exports = (client) => {
         commands[commandInfo.name] = commandFile;
         console.log(`Loaded ${commandInfo.name}.`);
 
-        // The fields that will be sent to discord to create the slash commands.
-        cmdFields = {
-            name: commandInfo.name,
-            description: commandInfo.description,
-            options: commandInfo.options ? commandInfo.options : []
-        }
-
-        if (production_mode) {
-            // Add the command to bot globally. (takes time to register)
-            client.application.commands.create(cmdFields);
-        } else {
-            // Add the command to all of the development guilds. (registers immediately)
-            development_guilds.forEach(guild_id => {
-                const guild = client.guilds.cache.get(guild_id);
-                if (guild) guild.commands.create(cmdFields); 
-            });
-        }
-        
+        commandAPIData.push(commandInfo);
     }
+
+    // Send the slash commands to the discord API.
+    (async () => {
+        const rest = new REST({ version: '9' }).setToken('token');
+        try {
+            if (production_mode) {
+                await rest.put(
+                    Routes.applicationCommands(client.user.id),
+                    { body: commandAPIData },
+                );
+            } else {
+                development_guilds.forEach( async guild_id => {
+                    const guild = client.guilds.cache.get(guild_id);
+                    if (guild) {
+                        await rest.put(
+                            Routes.applicationGuildCommands(client.user.id, guild.id),
+                            { body: commandAPIData },
+                        );
+                    }
+                });
+            }
+        } catch (err) { console.error(err); }
+    });
+
 
     // When the bot receives an interaction.
     client.on('interactionCreate', async (interaction) => {
