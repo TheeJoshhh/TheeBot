441b219a5ab4f81b5b320ab0553d9bf557fddb9c
Joshua Billing <joshua.billing9@gmail.com>
2022-05-12 16:41:53 +1200

    Added Boilerplate License Notice

--- commands/info.js
+++ commands/info.js
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2022 Joshua Billing / TheeJoshhh
+
+    TheeBot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TheeBot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with TheeBot.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { color_theme, website, discord_server } = require('../config.json');
