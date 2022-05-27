990e296f1a3189624620cd75e3e9c248fd76bbfa
Joshua Billing <joshua.billing9@gmail.com>
2022-05-27 23:43:55 +1200

    Added queue remove subcommand

--- commands/queue.js
+++ commands/queue.js
@@ -24,13 +24,37 @@ module.exports = {
 
     info: new SlashCommandBuilder()
         .setName('queue')
-        .setDescription('View the music queue.')
+        .setDescription('View or change the music queue.')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('remove')
+                .setDescription('Remove songs from the queue.')
+                .addNumberOption(option => 
+                    option
+                        .setName('start_position')
+                        .setDescription('The queue position of the song(s) to remove.')
+                        .setRequired(true)
+                )
+                .addNumberOption(option => 
+                    option
+                        .setName('end_position')
+                        .setDescription('The last song in the range of songs being removed.')
+                        .setRequired(false)
+                )     
+        )
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName('list')
+                .setDescription('List the songs in queue.')
+        )
+
     ,
 
     run: async function (client, interaction) {
 
         // HANDLE COMMANDS.
         if (interaction.isCommand()) {
+
             const GuildPlayer = musicData.get(interaction.guild.id);
 
             // If there's no MusicPlayer for the guild.
@@ -51,7 +75,62 @@ module.exports = {
                 return;
             }
 
+            
+
+            let { _subcommand, _hoistedOptions: opts } = interaction.options;
+
+            // Handle the remove subcommand.
+            if (_subcommand == 'remove') {
+                const startPos = Math.round(opts[0].value);
+                const endPos = opts[1] ? Math.round(opts[1].value) : startPos;
+
+                // If this guild isn't controlling the music, return.
+                if (GuildPlayer.controller) {
+                    interaction.reply({
+                        content: 'Your server is linked to another server! Use the `unlink` command to regain control over the music.',
+                        ephemeral: true
+                    });
+                    return;
+                }
+
+                // If the end position is smaller than the start position, return.
+                if (endPos < startPos) {
+                    interaction.reply({
+                        content: 'The end position must be after the start position!',
+                        ephemeral: true
+                    });
+                    return;
+                }
+
+                // If there aren't any songs in the specified position, return.
+                if (endPos > GuildPlayer.queue.length - 1) {
+                    interaction.reply({
+                        content: 'There\'s no song(s) in the specified queue position!',
+                        ephemeral: true
+                    });
+                    return;
+                }
+
+                // The amount of songs being removed.
+                const amount = endPos - startPos + 1;
+
+                // The response that'll be sent to the user.
+                let response = '';
+
+                if (amount > 1) response = `${GuildPlayer.queue[startPos].name} and ${amount-1} other songs have been removed from the queue.`;
+                else response = `${GuildPlayer.queue[startPos].name} was removed from the queue.`;
+                
+                GuildPlayer.queue.splice(startPos, amount);
+
+                interaction.reply(response);
+                return;
+            }
+
+            // Defer the reply because this might take a while.
             await interaction.deferReply();
+
+            // Guard Clause: If subcommand isn't list, don't run anything else.
+            if (_subcommand !== 'list') return;
 
             let queue = GuildPlayer.queue.slice(); // Clone of the queue array.
             let guildName = interaction.guild.name; // The name of the guild / linked guild.
@@ -99,8 +178,8 @@ module.exports = {
             .addComponents(new MessageButton().setCustomId('previous').setLabel('Previous').setStyle('PRIMARY').setEmoji('⏮️'))
             .addComponents(new MessageButton().setCustomId('next').setLabel('Next').setStyle('PRIMARY').setEmoji('⏭️'))
             const disabledRow = new MessageActionRow()
-            .addComponents(new MessageButton().setCustomId('previous').setLabel('Previous').setStyle('PRIMARY').setEmoji('⏮️'))
-            .addComponents(new MessageButton().setCustomId('next').setLabel('Next').setStyle('PRIMARY').setEmoji('⏭️'))
+            .addComponents(new MessageButton().setCustomId('previous').setLabel('Previous').setStyle('PRIMARY').setEmoji('⏮️').setDisabled(true))
+            .addComponents(new MessageButton().setCustomId('next').setLabel('Next').setStyle('PRIMARY').setEmoji('⏭️').setDisabled(true))
             
             const response = await interaction.editReply({
                 embeds: [pages[currentPage-1].setFooter({ text: `Page ${currentPage} of ${pages.length}` })],
